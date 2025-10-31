f9aff0b719ae374d3c18b16add87fcbd8a5e55d8
rahulsrs <rahulacharya2003@gmail.com>
2025-10-31 20:01:28 +0530

    initial commit

---
+++ src/main/java/io/github/rahulsrs/aichat/AiChat.java
@@ -0,0 +1,38 @@
+package io.github.rahulsrs.aichat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class AiChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("AI Chat Plugin has been enabled.");
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabled Plugin");
+    }
+
+    public class ChatListener implements Listener {
+//        ConsoleCommandSender console = Bukkit.getConsoleSender();
+        @EventHandler
+        public void onPlayerChat(AsyncPlayerChatEvent e){
+            String msg = e.getMessage();
+            getLogger().info("skibidi");
+            getLogger().info(msg);
+        }
+    }
+
+
+}
+
+
